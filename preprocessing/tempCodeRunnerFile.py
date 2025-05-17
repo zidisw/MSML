06bf5dcecc9f7c4cafb3622f24744bac22058b46
zidisw <wijaogyzis20d@student.unhas.ac.id>
2025-05-18 00:09:49 +0800

    first commit

---
+++ preprocessing/tempCodeRunnerFile.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+def preprocess_data(input_path, output_path):
+    # Memuat dataset
+    df = pd.read_csv(input_path)
+    
+    # Penanganan nilai negatif
+    df['SO2'] = df['SO2'].clip(lower=0)
+    df['PM10'] = df['PM10'].clip(lower=0)
+    
+    # Penanganan outlier
+    def remove_outliers(df, column):
+        Q1 = df[column].quantile(0.25)
+        Q3 = df[column].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        df = df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
+        return df
+    
+    df = remove_outliers(df, 'PM2.5')
+    df = remove_outliers(df, 'PM10')
+    
+    # Encoding Air Quality
+    label_encoder = LabelEncoder()
+    df['Air Quality'] = label_encoder.fit_transform(df['Air Quality'])
+    
+    # Normalisasi fitur numerik
+    numerical_cols = ['Temperature', 'Humidity', 'PM2.5', 'PM10', 'NO2', 'SO2', 'CO', 'Proximity_to_Industrial_Areas', 'Population_Density']
+    scaler = StandardScaler()
+    df[numerical_cols] = scaler.fit_transform(df[numerical_cols])
+    
+    # Menyimpan dataset yang telah diproses
+    df.to_csv(output_path, index=False)
+    print(f'Dataset yang telah diproses disimpan sebagai {output_path}')
+    return df
+
+if __name__ == '__main__':
+    preprocess_data('./updated_pollution_dataset.csv', 'preprocessing/pollution_dataset_preprocessed.csv')
